f4badab85f9f4f9e833800d2f05c1974792706bf
MReyes82 <142033671+MReyes82@users.noreply.github.com>
2024-02-29 06:13:50 +0000

    Folder actualizado con ultima actividad ('Escuela', con mecanismos particulares)

--- Desktop/Taller_java/escuela/src/escuelaClases/Alumno.java
+++
@@ -1,8 +0,0 @@
-package escuelaClases;
-
-public class Alumno extends Persona
-{
-	private int matricula;
-	private String carrera;
-	private Grupo subGrupo;
-}
--- Desktop/Taller_java/escuela/src/escuelaClases/Maestro.java
+++
@@ -1,6 +0,0 @@
-package escuelaClases;
-
-public class Maestro 
-{
-
-}
